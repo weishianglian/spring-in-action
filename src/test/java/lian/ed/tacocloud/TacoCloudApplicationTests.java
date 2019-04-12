9ba38eb1ce8bb402fdb6ae2d7a6d1d90a3577596
Wei-Shiang Lian <weishianglian@gmail.com>
2019-04-12 17:34:20 +0100

    init and controller, view, test created

---
+++ src/test/java/lian/ed/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,17 @@
+package lian.ed.tacocloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoCloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
